5c3b2e98595119cadb018936edbf6869e52036b1
git config --global user.name your <serjtsimmerman@gmail.com>
2022-04-28 23:58:44 +0300

    Modify TestList

--- test/tl/src/tl.cc
+++ test/tl/src/tl.cc
@@ -9,6 +9,17 @@ struct ListDeleter {
     }
 };
 
+// Initialize dl_node_t with ptr to an allocated int with value
+template <typename T> dl_node_t NodeInitWithValue(const T value) {
+    dl_node_t node = dl_node_init(NULL);
+    int *ptr = static_cast<T *>(calloc(1, sizeof(int)));
+    *ptr = value;
+    dl_node_set_data(node, static_cast<void *>(ptr));
+    return node;
+}
+
+// All data is assumed to be allocated on heap with calloc or malloc and freed with free()
+// Use ints for tests
 class List {
     std::unique_ptr<std::remove_pointer<dl_list_t>::type, ListDeleter> list;
 
@@ -16,26 +27,32 @@ class List {
     List() : list(dl_list_init(), ListDeleter{}) {
     }
 
+    List(std::initializer_list<int> ilist) : List() {
+        for (auto v : ilist) {
+            dl_list_push_back(list.get(), NodeInitWithValue(v));
+        }
+    }
+
     operator dl_list_t() {
         return list.get();
     }
 };
 
-// SetUp and TearDown calling constructor and destructor of dl_list
-// All data is assumed to be allocated on heap with calloc or malloc and freed with free()
-// Use ints for tests
-class TestList : public ::testing::Test {
-  public:
-    List list;
-};
+template <typename T> bool NodeEqValue(dl_node_t node, T value) {
+    return (*static_cast<T *>(dl_node_get_data(node)) == value);
+}
+
+template <typename T> void ASSERT_NODE_EQ(dl_node_t node, T value) {
+    ASSERT_EQ(NodeEqValue(node, value), true);
+}
 
 // Compare list with a std::array of ints moving in the forward direction
-template <std::size_t N> void ASSERT_LIST_EQ_FORW(dl_list_t list, std::array<int, N> array) {
+template <typename T, std::size_t N> void ASSERT_LIST_EQ_FORW(dl_list_t list, std::array<T, N> array) {
     dl_node_t node = dl_list_get_first(list);
     std::size_t i = 0;
 
     while (node && i < array.size()) {
-        ASSERT_EQ(*static_cast<int *>(dl_node_get_data(node)), array.at(i++));
+        ASSERT_NODE_EQ(node, array.at(i++));
         node = dl_node_get_next(node);
     }
 
@@ -44,12 +61,12 @@ template <std::size_t N> void ASSERT_LIST_EQ_FORW(dl_list_t list, std::array<int
 }
 
 // Compare list with a std::array of ints moving in the backward direction
-template <std::size_t N> void ASSERT_LIST_EQ_BACK(dl_list_t list, std::array<int, N> array) {
+template <typename T, std::size_t N> void ASSERT_LIST_EQ_BACK(dl_list_t list, std::array<T, N> array) {
     dl_node_t node = dl_list_get_last(list);
     std::size_t i = array.size() - 1;
 
     while (node) {
-        ASSERT_EQ(*static_cast<int *>(dl_node_get_data(node)), array.at(i));
+        ASSERT_NODE_EQ(node, array.at(i));
         node = dl_node_get_prev(node);
         if (i == 0) {
             break;
@@ -61,21 +78,14 @@ template <std::size_t N> void ASSERT_LIST_EQ_BACK(dl_list_t list, std::array<int
     ASSERT_EQ(i, 0);
 }
 
-template <std::size_t N> void ASSERT_LIST_EQ(dl_list_t list, std::array<int, N> array) {
+template <typename T, std::size_t N> void ASSERT_LIST_EQ(dl_list_t list, std::array<T, N> array) {
     ASSERT_LIST_EQ_FORW(list, array);
     ASSERT_LIST_EQ_BACK(list, array);
 }
 
-// Initialize dl_node_t with ptr to an allocated int with value
-dl_node_t NodeInitWithValue(int value) {
-    dl_node_t node = dl_node_init(NULL);
-    int *ptr = static_cast<int *>(calloc(1, sizeof(int)));
-    *ptr = value;
-    dl_node_set_data(node, static_cast<void *>(ptr));
-    return node;
-}
+TEST(TestList, Test1) {
+    List list{};
 
-TEST_F(TestList, TestPush) {
     dl_list_push_back(list, NodeInitWithValue(1));
     dl_list_push_front(list, NodeInitWithValue(2));
     dl_list_push_back(list, NodeInitWithValue(3));
@@ -85,7 +95,8 @@ TEST_F(TestList, TestPush) {
     });
 }
 
-TEST_F(TestList, TestPop) {
+TEST(TestList, Test2) {
+    List list{};
     dl_node_t node1 = NodeInitWithValue(2);
 
     dl_list_push_back(list, NodeInitWithValue(1));
@@ -120,7 +131,8 @@ TEST_F(TestList, TestPop) {
     ASSERT_TRUE(dl_list_is_empty(list));
 }
 
-TEST_F(TestList, TestInsert) {
+TEST(TestList, Test3) {
+    List list{};
     dl_node_t node1 = NodeInitWithValue(1);
 
     dl_list_push_back(list, node1);
@@ -141,6 +153,53 @@ TEST_F(TestList, TestInsert) {
 
     ASSERT_LIST_EQ(list, std::array<int, 4>{
                              {4, 2, 3, 1}
+    });
+
+    dl_list_push_front(list, dl_list_pop_back(list));
+
+    ASSERT_LIST_EQ(list, std::array<int, 4>{
+                             {1, 4, 2, 3}
+    });
+}
+
+TEST(TestList, Test4) {
+    List list{};
+    dl_list_push_front(list, NodeInitWithValue(1));
+
+    dl_node_t node1 = dl_list_pop_back(list);
+    ASSERT_NODE_EQ(node1, 1);
+
+    dl_list_push_back(list, node1);
+    dl_list_push_back(list, NodeInitWithValue(2));
+    dl_list_push_back(list, NodeInitWithValue(3));
+
+    ASSERT_LIST_EQ(list, std::array<int, 3>{
+                             {1, 2, 3}
+    });
+
+    dl_list_insert_after(list, dl_list_get_last(list), NodeInitWithValue(4));
+
+    ASSERT_LIST_EQ(list, std::array<int, 4>{
+                             {1, 2, 3, 4}
+    });
+}
+
+TEST(TestList, Test5) {
+    List list{1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    ASSERT_LIST_EQ(list, std::array<int, 9>{
+                             {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    });
+
+    dl_node_t node1 = dl_list_get_first(list);
+    while (!NodeEqValue(node1, 5)) {
+        node1 = dl_node_get_next(node1);
+    }
+
+    dl_list_remove(list, node1);
+
+    ASSERT_LIST_EQ(list, std::array<int, 8>{
+                             {1, 2, 3, 4, 6, 7, 8, 9}
     });
 }
 
