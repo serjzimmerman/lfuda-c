f9d5865483e499f05331cb1b8051d64a71bf4927
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-05-14 12:24:40 +0300

    Function for dumping local list of node with same weight

--- lfuda/src/dump.c
+++ lfuda/src/dump.c
@@ -24,6 +24,38 @@ void dump_freq_links(freq_list_t freqlist, FILE *file) {
     fprintf(file, "\t}\n");
 }
 
+void dump_freq_node_list(freq_node_t freqnode, output_s *format_dump) {
+    assert(freqnode);
+    assert(format_dump);
+    assert(format_dump->file);
+    assert(format_dump->print);
+
+    FILE *file = format_dump->file;
+
+    fprintf(file, "\tsubgraph FREQ_%p\n", freqnode);
+    fprintf(file, "\t\trankdir = TB;\n\n");
+    fprintf(file, "\t\tfreq_%p [label = \"%u\", fillcolor = \"deepskyblue\", fontcolor = \"white\"];\n\n", freqnode,
+            freq_node_get_key(freqnode));
+
+    local_list_t local_list = freq_node_get_local(freqnode);
+    local_node_t current_local_node = dl_list_get_first(local_list);
+
+    while (!current_local_node) {
+        fprintf(file, "\t\tlocal_node_%p [label = \"%s\"]\n", current_local_node,
+                *(format_dump->print)(dl_node_get_data(current_local_node)));
+        current_local_node = dl_node_get_next(current_local_node);
+    }
+    fprintf(file, "\n");
+    fprintf(file, "\t\tfreq_%p", freqnode);
+    current_local_node = dl_list_get_first(local_list);
+    while (!current_local_node) {
+        fprintf(file, " -> local_node_%p", current_local_node);
+        current_local_node = dl_node_get_next(current_local_node);
+    }
+    fprintf(file, "\n");
+    fprintf(file, "\t}\n");
+}
+
 void dump_cache(void *cache_, output_s *format_dump) {
     assert(cache_);
     assert(format_dump);
