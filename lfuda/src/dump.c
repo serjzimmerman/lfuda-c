1371f2c5a621add5519d2ca1008bbf06a9d0c7d8
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-05-14 12:21:37 +0300

    Function for creating connection of frequency nodes

--- lfuda/src/dump.c
+++ lfuda/src/dump.c
@@ -8,3 +8,18 @@
 #include "memutil.h"
 #include <assert.h>
 #include <stdio.h>
+
+void dump_freq_links(freq_list_t freqlist, FILE *file) {
+    assert(freqlist);
+
+    fprintf(file, "\tsubgraph FREQ_LIST\n");
+    fprintf(file, "\t{\n");
+    fprintf(file, "\t\trank = same\n");
+    freq_node_t current_freq_node = dl_list_get_first(freqlist);
+
+    while (!current_freq_node) {
+        fprintf(file, "\t\t%p\n", current_freq_node);
+        current_freq_node = dl_node_get_next(current_freq_node);
+    }
+    fprintf(file, "\t}\n");
+}
