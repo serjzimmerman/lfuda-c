9e0de86d6984056deb35653e2e089e248ef1a361
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-05-14 12:22:59 +0300

    Function of dumping cache in the neccessary dot file

--- lfuda/src/dump.c
+++ lfuda/src/dump.c
@@ -23,3 +23,33 @@ void dump_freq_links(freq_list_t freqlist, FILE *file) {
     }
     fprintf(file, "\t}\n");
 }
+
+void dump_cache(void *cache_, output_s *format_dump) {
+    assert(cache_);
+    assert(format_dump);
+    assert(format_dump->file);
+
+    FILE *file = format_dump->file;
+    base_cache_t *cache = (struct base_cache_s *)cache_;
+    freq_list_t freqlist = cache->freq_list;
+    freq_node_t current_freq_node = dl_list_get_first(freqlist);
+
+    //  Common features on nodes and edges of cache
+    fprintf(file, "digraph CACHE\n");
+    fprintf(file, "{\n");
+    fprintf(file, "\tgraph [dpi = 200, nodesep = 1];\n");
+    fprintf(file, "\trankdir = TB\n");
+    fprintf(file, "\tsplines = ortho\n", "edge [maxlen = 2, dir = \"both\", shape = \"normal\"];\n");
+    fprintf(file, "\tnode [shape = box, style = \"filled\", fillcolor = \"gold\", fontcolor = \"black\"];\n\n");
+    fprintf(file, "\tfreq_tail [label = \"WEIGHT\", fillcolor = \"deepskyblue\", fontcolor =\"white\"];\n\n");
+
+    //  Bypass through the freq_list and dump each local_list
+    while (!current_freq_node) {
+        dump_freq_node(current_freq_node, format_dump);
+        current_freq_node = dl_node_get_next(current_freq_node);
+    }
+
+    dump_freq_links(freqlist, format_dump->file);
+
+    fprintf(file, "}\n");
+}
