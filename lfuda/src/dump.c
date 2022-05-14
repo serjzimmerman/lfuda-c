8f35179353026f3195a360052961047616e46ea3
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2022-05-14 13:16:09 +0300

    Fixes naming and bug with condition in circles

--- lfuda/src/dump.c
+++ lfuda/src/dump.c
@@ -14,17 +14,17 @@ void dump_freq_links(freq_list_t freqlist, FILE *file) {
 
     fprintf(file, "\tsubgraph FREQ_LIST\n");
     fprintf(file, "\t{\n");
-    fprintf(file, "\t\trank = same\n");
+    fprintf(file, "\t\trank = same;\n");
     freq_node_t current_freq_node = dl_list_get_first(freqlist);
 
-    while (!current_freq_node) {
-        fprintf(file, "\t\t%p\n", current_freq_node);
+    while (current_freq_node) {
+        fprintf(file, "\t\t%p;\n", current_freq_node);
         current_freq_node = dl_node_get_next(current_freq_node);
     }
     fprintf(file, "\t}\n");
 }
 
-void dump_freq_node_list(freq_node_t freqnode, output_s *format_dump) {
+void dump_freq_node_list(freq_node_t freqnode, output_t *format_dump) {
     assert(freqnode);
     assert(format_dump);
     assert(format_dump->file);
@@ -40,16 +40,17 @@ void dump_freq_node_list(freq_node_t freqnode, output_s *format_dump) {
     local_list_t local_list = freq_node_get_local(freqnode);
     local_node_t current_local_node = dl_list_get_first(local_list);
 
-    while (!current_local_node) {
+    while (current_local_node) {
         fprintf(file, "\t\tlocal_node_%p [label = \"", current_local_node);
-        (format_dump->print)(dl_node_get_data(current_local_node), file);
+        local_node_data_t current_local_node_data = dl_node_get_fam(current_local_node);
+        (format_dump->print)(current_local_node_data.index, file);
         fprintf(file, "\"\n");
         current_local_node = dl_node_get_next(current_local_node);
     }
     fprintf(file, "\n");
     fprintf(file, "\t\tfreq_%p", freqnode);
     current_local_node = dl_list_get_first(local_list);
-    while (!current_local_node) {
+    while (current_local_node) {
         fprintf(file, " -> local_node_%p", current_local_node);
         current_local_node = dl_node_get_next(current_local_node);
     }
@@ -57,7 +58,7 @@ void dump_freq_node_list(freq_node_t freqnode, output_s *format_dump) {
     fprintf(file, "\t}\n");
 }
 
-void dump_cache(void *cache_, output_s *format_dump) {
+void dump_cache(void *cache_, output_t *format_dump) {
     assert(cache_);
     assert(format_dump);
     assert(format_dump->file);
@@ -71,13 +72,13 @@ void dump_cache(void *cache_, output_s *format_dump) {
     fprintf(file, "digraph CACHE\n");
     fprintf(file, "{\n");
     fprintf(file, "\tgraph [dpi = 200, nodesep = 1];\n");
-    fprintf(file, "\trankdir = TB\n");
+    fprintf(file, "\trankdir = TB;\n");
     fprintf(file, "\tsplines = ortho\n", "edge [maxlen = 2, dir = \"both\", shape = \"normal\"];\n");
     fprintf(file, "\tnode [shape = box, style = \"filled\", fillcolor = \"gold\", fontcolor = \"black\"];\n\n");
     fprintf(file, "\tfreq_tail [label = \"WEIGHT\", fillcolor = \"deepskyblue\", fontcolor =\"white\"];\n\n");
 
     //  Bypass through the freq_list and dump each local_list
-    while (!current_freq_node) {
+    while (current_freq_node) {
         dump_freq_node(current_freq_node, format_dump);
         current_freq_node = dl_node_get_next(current_freq_node);
     }
